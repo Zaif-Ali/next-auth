3d0396f4a456c71417c896c1e473805a39607620
Huzaifa <zaifali785@gmail.com>
2023-05-31 19:21:51 +0500

    Style user card

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -4,36 +4,27 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 interface Props {
-  EachUser : IUser
+  EachUser: IUser;
 }
 
-const UserCard: NextPage<Props> = ({EachUser}) => {
-  const { data: session } = useSession();
+const UserCard: NextPage<Props> = ({ EachUser }) => {
   return (
     <>
-      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full max-w-sm  border dark:border-0 border-gray-200 rounded-lg shadow-xl  dark:border-gray-700 dark:bg-gradient-to-b dark:from-[#8E2DE2] dark:to-[#4A00E0] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-100">
         <div className="flex flex-col items-center pb-5 py-2">
           <Image
-            className="w-20 h-20 mb-3 rounded-full shadow-lg"
+            className="w-16 h-16 lg:w-20 lg:h-20 mb-3 rounded-full shadow-lg"
             src={EachUser.image}
             alt={EachUser.name}
             width={96}
             height={96}
           />
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+          <h5 className="mb-1 text-base lg:text-xl font-medium text-gray-900 dark:text-white">
             {EachUser.name}
           </h5>
-          <span className="text-sm text-gray-500 dark:text-gray-400">
+          <span className="text-xs lg:text-sm text-gray-900 dark:text-gray-300">
             {EachUser.email}
           </span>
-          <div className="flex mt-4 space-x-3 md:mt-6">
-            <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Add friend
-            </button>
-            <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
-              Veiw Profile
-            </button>
-          </div>
         </div>
       </div>
     </>
--- pages/admin/dashboard/VeiwUsers.tsx
+++ pages/admin/dashboard/VeiwUsers.tsx
@@ -14,9 +14,14 @@ const VeiwUsers: NextPage<Props> = ({}) => {
     <Wrapper>
       <div>{error}</div>
       {loading ? (
-        <div>Loading...</div>
+        <div className="flex flex-col items-center pb-5 py-2">
+          <div className="w-20 h-20 mb-3 rounded-full bg-gray-300"></div>
+          <div className="w-32 h-4 mt-2 bg-gray-300 rounded-full"></div>
+          <div className="w-24 h-4 mt-2 bg-gray-300 rounded-full"></div>
+          <div className="w-32 h-4 mt-2 bg-gray-300 rounded-full"></div>
+        </div>
       ) : (
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-3">
           {users?.map((EachUser, key) => {
             return <UserCard key={key} EachUser={EachUser} />;
           })}
