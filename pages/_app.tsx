61ff2086f56ee5f0b821bff278e4b783d3b00be2
Huzaifa <zaifali785@gmail.com>
2023-06-30 00:56:50 +0500

    welcome react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,23 +6,29 @@ import Layout from "@/layout/layout";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+// Create a client
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate persistor={PersistStore}>
-        <ThemeProvider enableSystem={true} attribute="class">
-          <SessionProvider>
-            <Header />
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-            <ThemeChange />
-          </SessionProvider>
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate persistor={PersistStore}>
+          <ThemeProvider enableSystem={true} attribute="class">
+            <SessionProvider>
+              <Header />
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+              <ThemeChange />
+            </SessionProvider>
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
   );
 }
